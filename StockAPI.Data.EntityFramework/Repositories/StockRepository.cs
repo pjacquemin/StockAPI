1c3482f50089bb1d09fb2e7ba82a6ef768063903
Patrick Jacquemin <patrickjacquemin@gmail.com>
2023-12-07 15:50:24 +0100

    useless "using" removed

--- StockAPI.Data.EntityFramework/Repositories/StockRepository.cs
+++ StockAPI.Data.EntityFramework/Repositories/StockRepository.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using StockAPI.Interfaces;
 using StockAPI.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StockAPI.Data.EntityFramework.Repositories
 {
--- StockAPI.Logic/StockService/StockService.cs
+++ StockAPI.Logic/StockService/StockService.cs
@@ -1,10 +1,5 @@
 ﻿using StockAPI.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace StockAPI.Logic.StockService
